09eb1a05afc33b470e250d6888bc8c4fcc42102b
atanoto-pact <79881575+atanoto-pact@users.noreply.github.com>
2021-03-01 17:16:26 -0800

    Create Validate Worklist And Storage2.cs

---
+++ Validate Worklist And Storage2.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using BioSero.GreenButtonGo.Scripting;
+
+namespace GreenButtonGo.Scripting
+{
+    public class Validate_Worklist_And_Storage2 : BioSero.GreenButtonGo.GBGScript
+    {
+
+        public void Run(Dictionary<String, Object> variables, RuntimeInfo runtimeInfo)
+        {
+            // Insert code here:
+            List<string[]> echoTable1 = Database.GetAllRows("Identifiers_Worklist_Source", new string[] {"Barcode"}).ToList();
+            List<string[]> echoTable2 = Database.GetAllRows("Identifiers_Worklist_Dest", new string[] {"Barcode"}).ToList();
+            
+            echoTable1.AddRange(echoTable2);
+           
+            List<string[]> storageTable = Database.GetAllRows("Plate_Storage_Hotels", new string[] {"Barcode"}).ToList();
+            foreach(var line in echoTable1)
+            {
+                string Barcode = line[0];
+                //MessageBox.Show(sourceBarcode + "     " + destinationBarcode);
+                if(!storageTable.Any(x=>x[0] == Barcode))
+                {
+                    throw new InvalidOperationException("Could not find a plate in storage with barcode '" + Barcode + "'");
+                }
+             }
+            
+        }
+    }
+}
